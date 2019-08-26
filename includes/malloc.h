bd6fabb5496e750d0fb9a0c713177394448f30f7
Joffrey Janiec <jjaniec@student.42.fr>
2019-08-26 20:54:19 +0200

    ft_prefix, rename files, update Makefile & header

--- includes/malloc.h
+++ includes/malloc.h
@@ -3,23 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   malloc.h                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eparisot <eparisot@42.student.fr>          +#+  +:+       +#+        */
+/*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/23 17:48:14 by eparisot          #+#    #+#             */
-/*   Updated: 2019/08/24 14:34:29 by eparisot         ###   ########.fr       */
+/*   Updated: 2019/08/26 20:36:25 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MALLOC_H
-# define MALLOC_H
+#define MALLOC_H
 
-#include "../libft/srcs/libft.h"
+#include <libft.h>
 #include <unistd.h>
 #include <sys/mman.h>
 
-void				free(void *ptr);
-void				*malloc(size_t size);
-void				*realloc(void *ptr, size_t size);
-void				show_alloc_mem(void);
+void ft_free(void *ptr);
+void *ft_malloc(size_t size);
+void *ft_realloc(void *ptr, size_t size);
+void show_alloc_mem(void);
 
 #endif
