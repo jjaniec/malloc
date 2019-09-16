48b6828d9a8cd7fcec87fb8de7114939aa989b53
Joffrey Janiec <jjaniec@student.42.fr>
2019-09-16 16:07:18 +0200

    Begin ft_malloc

--- tests/main.c
+++ tests/main.c
@@ -3,10 +3,15 @@
 
 int main()
 {
-	plan(1);
+	// plan(1);
 	// free(NULL);
 	// ok(1 != 0, "test");
 	ok(NULL != ft_malloc(2), "NULL != malloc(2)");
+	ok(NULL != ft_malloc(256), "NULL != malloc(256)");
+	char * tmp;
+	tmp = ft_malloc(25);
+	tmp[0] = 'a';
+	ok(tmp[0] == 'a', "Test write");
 	done_testing();
 	return 0;
 }
