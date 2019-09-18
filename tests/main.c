2cc291f833b62406c316e72a638ef3df8919ac5f
Joffrey Janiec <jjaniec@student.42.fr>
2019-09-18 16:42:18 +0200

    add show_alloc_mem.c

--- tests/main.c
+++ tests/main.c
@@ -18,7 +18,7 @@ int			main()
 	char	*tmp2;
 	int		i;
 
-	tmp = malloc(2);
+	tmp = ft_malloc(2);
 	ok(NULL != tmp, "Malloc - Tiny 1 - Allocation");
 	tmp[0] = 'a';
 	tmp[1] = '\0';
@@ -27,9 +27,9 @@ int			main()
 
 	tmp = ft_malloc(TINY_MAX_SIZE / 2 + 1);
 	ok(NULL != tmp, "Malloc - Tiny 2 - Allocation");
-	ft_memset(tmp, 'A', TINY_MAX_SIZE / 2 - 1);
+	ft_memset(tmp, 'b', TINY_MAX_SIZE / 2 - 1);
 	tmp[TINY_MAX_SIZE / 2] = '\0';
-	ok(read_str_char(tmp, 'A') == 0, "Malloc - Tiny 2 - String Assignation");
+	ok(read_str_char(tmp, 'b') == 0, "Malloc - Tiny 2 - String Assignation");
 
 	tmp = ft_malloc(TINY_MAX_SIZE / 2 + 1);
 	ok(NULL != tmp, "Malloc - Tiny 3 - Allocation on different page");
@@ -39,17 +39,17 @@ int			main()
 
 	tmp = ft_malloc(SMALL_MAX_SIZE / 2 + 1);
 	ok(NULL != tmp, "Malloc - Small 1 - Basic allocation");
-	ft_memset(tmp, 'A', SMALL_MAX_SIZE / 2 - 1);
+	ft_memset(tmp, 'B', SMALL_MAX_SIZE / 2 - 1);
 	tmp[SMALL_MAX_SIZE / 2] = '\0';
-	ok(read_str_char(tmp, 'A') == 0, "Malloc - Small 1 - String assignation");
+	ok(read_str_char(tmp, 'B') == 0, "Malloc - Small 1 - String assignation");
 
 	tmp = ft_malloc(SMALL_MAX_SIZE * 2 + 1);
 	ok(NULL != tmp, "Malloc - Big 1 - Basic allocation");
-	ft_memset(tmp, 'A', SMALL_MAX_SIZE * 2 - 1);
+	ft_memset(tmp, 'C', SMALL_MAX_SIZE * 2 - 1);
 	tmp[SMALL_MAX_SIZE * 2] = '\0';
-	ok(read_str_char(tmp, 'A') == 0, "Malloc - Big 1 - String assignation");
+	ok(read_str_char(tmp, 'C') == 0, "Malloc - Big 1 - String assignation");
 
-
+	show_alloc_mem();
 
 	done_testing();
 	return 0;
