f51c12c3a88dd416caf3fcd24bd1391c8b781bb3
Joffrey Janiec <jjaniec@student.42.fr>
2019-09-19 15:22:47 +0200

    Begin free / free_all for tiny mallocs

--- includes/malloc.h
+++ includes/malloc.h
@@ -6,50 +6,52 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/08/23 17:48:14 by jjaniec           #+#    #+#             */
-/*   Updated: 2019/09/18 15:42:49 by jjaniec          ###   ########.fr       */
+/*   Updated: 2019/09/19 13:47:07 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef MALLOC_H
-#define MALLOC_H
+#ifndef FT_MALLOC_H
+# define FT_MALLOC_H
 
-#include <libft.h>
-#include <unistd.h>
-#include <sys/mman.h>
-#include <stdbool.h>
-#include <stdlib.h>
+# include <libft.h>
+# include <unistd.h>
+# include <sys/mman.h>
+# include <stdbool.h>
+# include <stdlib.h>
 
 // Allocations max size per allocation type
-#define TINY_MAX_SIZE 256
-#define SMALL_MAX_SIZE 4096
+# define TINY_MAX_SIZE 256
+# define SMALL_MAX_SIZE 4096
 
 // Tiny & small pages size ( x * getpagesize() )
-#define TINY_PAGE_SIZE 8
-#define SMALL_PAGE_SIZE 24
+# define TINY_PAGE_SIZE 8
+# define SMALL_PAGE_SIZE 24
 
 // Initial number of pages in tiny & small page regions
-#define TINY_REGION_PAGE_COUNT 5
-#define SMALL_REGION_PAGE_COUNT 5
+# define TINY_REGION_PAGE_COUNT 1
+# define SMALL_REGION_PAGE_COUNT 1
 
 // mmap parameters for pages allocations
-#define PAGE_MMAP_PROT PROT_READ | PROT_WRITE | PROT_EXEC
-#define PAGE_MMAP_FLAGS MAP_ANON | MAP_PRIVATE
+# define PAGE_MMAP_PROT PROT_READ | PROT_WRITE | PROT_EXEC
+# define PAGE_MMAP_FLAGS MAP_ANON | MAP_PRIVATE
 
 //
-#define SHOW_ALLOC_MEM_BYTES_PER_LINE	16
-#define SHOW_ALLOC_MEM_FREE_BLOCKS false
+# define SHOW_ALLOC_MEM_BYTES_PER_LINE	16
+# define SHOW_ALLOC_MEM_FREE_BLOCKS false
 
-typedef struct s_malloc_header
+typedef						struct s_malloc_header
 {
-	bool free;
-	unsigned int size;
-	struct s_malloc_header *prev;
-	struct s_malloc_header *next;
-} t_malloc_header;
+	bool					free : 1;
+	char					_unused[3];
+	unsigned int			size;
+	struct s_malloc_header	*prev;
+	struct s_malloc_header	*next;
+}							t_malloc_header;
 
-void ft_free(void *ptr);
-void *ft_malloc(size_t size);
-void *ft_realloc(void *ptr, size_t size);
-void show_alloc_mem(void);
+void						ft_free(void *ptr);
+void						*ft_malloc(size_t size);
+void						*ft_realloc(void *ptr, size_t size);
+void						show_alloc_mem(void);
+void						ft_free_all(void);
 
 #endif
--- tests/main.c
+++ tests/main.c
@@ -18,18 +18,20 @@ int			main()
 	char	*tmp2;
 	int		i;
 
-	tmp = ft_malloc(2);
+	tmp = ft_malloc(5);
 	ok(NULL != tmp, "Malloc - Tiny 1 - Allocation");
 	tmp[0] = 'a';
 	tmp[1] = '\0';
 	ok(tmp[0] == 'a', "Malloc - Tiny 1 - Assignation");
 	ok(tmp[1] == '\0', "Malloc - Tiny 1 - Assignation 2");
+	// ft_free(tmp);
 
 	tmp = ft_malloc(TINY_MAX_SIZE / 2 + 1);
 	ok(NULL != tmp, "Malloc - Tiny 2 - Allocation");
 	ft_memset(tmp, 'b', TINY_MAX_SIZE / 2 - 1);
 	tmp[TINY_MAX_SIZE / 2] = '\0';
 	ok(read_str_char(tmp, 'b') == 0, "Malloc - Tiny 2 - String Assignation");
+	// ft_free(tmp);
 
 	tmp = ft_malloc(TINY_MAX_SIZE / 2 + 1);
 	ok(NULL != tmp, "Malloc - Tiny 3 - Allocation on different page");
@@ -37,18 +39,24 @@ int			main()
 	tmp[TINY_MAX_SIZE / 2] = '\0';
 	ok(read_str_char(tmp, 'A') == 0, "Malloc - Tiny 3 - String Assignation on different page");
 
-	tmp = ft_malloc(SMALL_MAX_SIZE / 2 + 1);
-	ok(NULL != tmp, "Malloc - Small 1 - Basic allocation");
-	ft_memset(tmp, 'B', SMALL_MAX_SIZE / 2 - 1);
-	tmp[SMALL_MAX_SIZE / 2] = '\0';
-	ok(read_str_char(tmp, 'B') == 0, "Malloc - Small 1 - String assignation");
+	// tmp = ft_malloc(SMALL_MAX_SIZE / 2 + 1);
+	// ok(NULL != tmp, "Malloc - Small 1 - Basic allocation");
+	// ft_memset(tmp, 'B', SMALL_MAX_SIZE / 2 - 1);
+	// tmp[SMALL_MAX_SIZE / 2] = '\0';
+	// ok(read_str_char(tmp, 'B') == 0, "Malloc - Small 1 - String assignation");
 
-	tmp = ft_malloc(SMALL_MAX_SIZE * 2 + 1);
-	ok(NULL != tmp, "Malloc - Big 1 - Basic allocation");
-	ft_memset(tmp, 'C', SMALL_MAX_SIZE * 2 - 1);
-	tmp[SMALL_MAX_SIZE * 2] = '\0';
-	ok(read_str_char(tmp, 'C') == 0, "Malloc - Big 1 - String assignation");
+	// tmp = ft_malloc(SMALL_MAX_SIZE * 2 + 1);
+	// ok(NULL != tmp, "Malloc - Big 1 - Basic allocation");
+	// ft_memset(tmp, 'C', SMALL_MAX_SIZE * 2 - 1);
+	// tmp[SMALL_MAX_SIZE * 2] = '\0';
+	// ok(read_str_char(tmp, 'C') == 0, "Malloc - Big 1 - String assignation");
 
+	show_alloc_mem();
+
+	// ft_free(tmp);
+	// show_alloc_mem();
+
+	ft_free_all();
 	show_alloc_mem();
 
 	done_testing();
