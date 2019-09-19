b0f73ac8719314f6b53e5598c90af979df861f06
Joffrey Janiec <jjaniec@student.42.fr>
2019-09-19 14:59:19 +0200

    Fix show_alloc_mem leak from ft_itoa_base

--- srcs/show_alloc_mem.c
+++ srcs/show_alloc_mem.c
@@ -6,13 +6,13 @@
 /*   By: jjaniec <jjaniec@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/18 15:14:11 by jjaniec           #+#    #+#             */
-/*   Updated: 2019/09/18 16:35:32 by jjaniec          ###   ########.fr       */
+/*   Updated: 2019/09/19 14:10:58 by jjaniec          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <malloc.h>
 
-extern void	*g_alloc_mem[2];
+extern t_malloc_header	*g_alloc_mem[2];
 
 static void	print_alloc_data(t_malloc_header *header)
 {
@@ -20,6 +20,7 @@ static void	print_alloc_data(t_malloc_header *header)
 	unsigned int	j;
 	char			cur_byte_value;
 	unsigned int	line_start_offset;
+	char			*byte_hex_fmt;
 
 	i = 0;
 	while (i < header->size)
@@ -33,7 +34,9 @@ static void	print_alloc_data(t_malloc_header *header)
 			cur_byte_value = ((char *)(header + sizeof(t_malloc_header)))[i];
 			if (cur_byte_value < 10)
 				ft_putchar('0');
-			ft_putstr(ft_itoa_base(cur_byte_value, 16));
+			byte_hex_fmt = ft_itoa_base(cur_byte_value, 16);
+			ft_putstr(byte_hex_fmt);
+			free(byte_hex_fmt);
 			if (i % 2)
 				ft_putchar(' ');
 			i += 1;
@@ -52,7 +55,7 @@ static void	show_alloc_mem_list(char *type, t_malloc_header *start)
 {
 	while (start)
 	{
-		printf("%s block of size %u @%p\n", type, start->size, start);
+		printf("%s block of size %u @%p, free: %d\n", type, start->size, start, start->free);
 		if (SHOW_ALLOC_MEM_FREE_BLOCKS || !start->free)
 		{
 			print_alloc_data(start);
@@ -64,6 +67,8 @@ static void	show_alloc_mem_list(char *type, t_malloc_header *start)
 
 void		show_alloc_mem(void)
 {
+	printf("===== show_alloc_mem =====\n");
 	show_alloc_mem_list("Tiny", g_alloc_mem[0]);
 	show_alloc_mem_list("Small", g_alloc_mem[1]);
+	printf("==========================\n");
 }
